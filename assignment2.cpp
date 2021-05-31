908ed21159bdae878a43aa2c8f8d1211aa15ecc6
Detective954 <ashu954688@gmail.com>
2021-05-31 14:04:11 +0530

    feature2

--- assignment2.cpp
+++ assignment2.cpp
@@ -1,5 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+int firstOccPositve(int arr[], int low, int high)
+{
+	if(high >= low)
+    {
+        int mid = (low + high)/2;
+        if(mid == arr[mid]){
+            return mid;
+        }
+        if(mid > arr[mid]){
+             return firstOccPositve(arr, (mid + 1), high);
+        }
+
+        else{
+            return firstOccPositve(arr, low, (mid -1));
+        }
+    }
+    return -1;
+}
+void removeDuplicate(vector<int> &v){
+    vector<int>::iterator it;
+    it=unique(v.begin(),v.end());
+    v.resize(distance(v.begin(),it));
+}
 int main(){
     int n;
     cout<<"Enter the size of array : "<<endl;
